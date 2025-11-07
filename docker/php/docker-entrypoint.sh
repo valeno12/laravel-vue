b397ad9cf43e80fe03db322729937c1e9a2795e8
Valentino notebook <vvalentinogm@gmail.com>
2025-11-07 16:27:40 -0300

    entrypoint

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e 
+set -e
 
-# La única lógica necesaria en runtime que no está resuelta por la imagen base
-echo "--- Aplicando permisos de Laravel (Storage/Cache) ---"
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+echo "--- Aplicando permisos de Laravel ---"
+# Dar propiedad a www-data y permisos 775 a todo el proyecto
+chown -R www-data:www-data /var/www/html
+chmod -R 775 /var/www/html
 echo "--- Permisos Aplicados ---"
 
-# Ejecuta el CMD que se le pasó (e.g., php-fpm)
 exec "$@"
